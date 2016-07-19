a164b18920142b224d3326bcd11878ef65264aee
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-19 12:18:39 +0100

    Test the tokens passed individually

--- spec/twitter_list/scraper_spec.rb
+++ spec/twitter_list/scraper_spec.rb
@@ -21,4 +21,30 @@ describe TwitterList::Scraper do
     end
   end
 
+  describe "when using the four tokens separately" do
+    let(:tokens) {{
+      consumer_key: 'abc',
+      consumer_secret: '123',
+      access_token: 'def',
+      access_token_secret: '456'
+    }}
+    let(:scraper) {TwitterList::Scraper.new(tokens)}
+
+    it "assigns the consumer key correctly" do
+      expect(scraper.client.consumer_key).to eq('abc')
+    end
+
+    it "assigns the consumer secret correctly" do
+      expect(scraper.client.consumer_secret).to eq('123')
+    end
+
+    it "assigns the access token correctly" do
+      expect(scraper.client.access_token).to eq('def')
+    end
+
+    it "assigns the access secret correctly" do
+      expect(scraper.client.access_token_secret).to eq('456')
+    end
+  end
+
 end
