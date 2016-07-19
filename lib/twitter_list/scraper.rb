c97603a09ff5ec4cfef689ee40ee390f5ba348ee
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-19 12:15:41 +0100

    Test the tokens passed in one variable

--- lib/twitter_list/scraper.rb
+++ lib/twitter_list/scraper.rb
@@ -3,15 +3,27 @@ module TwitterList
   class Scraper
     attr_accessor :user, :list, :client
 
-    def initialize(client)
-      @client = client do |config|
-        if ENV.key? 'MORPH_TWITTER_TOKENS'
-          consumer_key, consumer_secret, access_token, access_secret = ENV['MORPH_TWITTER_TOKENS'].split('|')
-        end
-        config.consumer_key        = ENV['MORPH_TWITTER_CONSUMER_KEY']        || consumer_key
-        config.consumer_secret     = ENV['MORPH_TWITTER_CONSUMER_SECRET']     || consumer_secret
-        config.access_token        = ENV['MORPH_TWITTER_ACCESS_TOKEN']        || access_token
-        config.access_token_secret = ENV['MORPH_TWITTER_ACCESS_TOKEN_SECRET'] || access_secret
+    def initialize(tokens)
+      if tokens.has_key? :twitter_tokens
+        consumer_key, consumer_secret, access_token, access_secret = tokens[:twitter_tokens].split('|')
+      end
+
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = tokens[:consumer_key]        || consumer_key
+        config.consumer_secret     = tokens[:consumer_secret]     || consumer_secret
+        config.access_token        = tokens[:access_token]        || access_token
+        config.access_token_secret = tokens[:access_token_secret] || access_secret
+      end
+    end
+
+    def people(user, list)
+      client.list_members(user, list).map do |person|
+        {
+          id: person.id,
+          name: person.name,
+          twitter: person.screen_name,
+          image: person.default_profile_image? ? '' : person.profile_image_url_https(:original).to_s,
+        }
       end
     end
 
--- spec/twitter_list/scraper_spec.rb
+++ spec/twitter_list/scraper_spec.rb
@@ -1,22 +1,24 @@
 describe TwitterList::Scraper do
 
-  let(:client)  {Twitter::REST::Client.new}
-  let(:scraper) {TwitterList::Scraper.new(client)}
+  describe "when using the four tokens at once" do
+    let(:tokens) {'abc|123|def|456'}
+    let(:scraper) {TwitterList::Scraper.new(twitter_tokens: tokens)}
 
-  it "assigns the consumer key correctly" do
-    expect(client.consumer_key).to eq(ENV['MORPH_TWITTER_CONSUMER_KEY'])
-  end
+    it "assigns the consumer key correctly" do
+      expect(scraper.client.consumer_key).to eq('abc')
+    end
 
-  it "assigns the consumer secret correctly" do
-    expect(client.consumer_key).to eq(ENV['MORPH_TWITTER_CONSUMER_SECRET'])
-  end
+    it "assigns the consumer secret correctly" do
+      expect(scraper.client.consumer_secret).to eq('123')
+    end
 
-  it "assigns the access token correctly" do
-    expect(client.consumer_key).to eq(ENV['MORPH_TWITTER_ACCESS_TOKEN'])
-  end
+    it "assigns the access token correctly" do
+      expect(scraper.client.access_token).to eq('def')
+    end
 
-  it "assigns the access secret correctly" do
-    expect(client.consumer_key).to eq(ENV['MORPH_TWITTER_ACCESS_TOKEN_SECRET'])
+    it "assigns the access secret correctly" do
+      expect(scraper.client.access_token_secret).to eq('456')
+    end
   end
 
 end
